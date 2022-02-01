8e5353de70272a9422bc54b3ee9faa114b9e8d74
Denize <DenizeScheffold@gmail.com>
2022-02-01 10:03:41 +0100

    remove previous module

--- sti--denise-jonathan-denize-/se.sti.stidenisejonathandenize.domain/src/main/java/se/sti/stidenisejonathandenize/domain/Computer.java
+++
@@ -1,7 +0,0 @@
-package se.sti.stidenisejonathandenize.domain;
-
-public class Computer {
-
-    private String mac;
-    private String pc;
-}
--- sti--denise-jonathan-denize-/se.sti.stidenisejonathandenize.domain/src/main/java/se/sti/stidenisejonathandenize/domain/StudentRepository.java
+++
@@ -1,9 +0,0 @@
-package se.sti.stidenisejonathandenize.domain;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface StudentRepository extends JpaRepository <Student, String>{
-
-}
