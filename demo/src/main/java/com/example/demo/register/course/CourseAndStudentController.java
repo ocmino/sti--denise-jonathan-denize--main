d7e1fdc8c773c52b492f29d94f835a78d00fac7e
Jonathan Lindqvist <ocmino@gmail.com>
2022-02-02 10:11:15 +0100

    .

--- demo/src/main/java/com/example/demo/register/course/CourseAndStudentController.java
+++ demo/src/main/java/com/example/demo/register/course/CourseAndStudentController.java
@@ -1,6 +1,5 @@
 package com.example.demo.register.course;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.GetMapping;
