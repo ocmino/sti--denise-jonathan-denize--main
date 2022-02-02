d22284409a441a3c5c746b8ce655d76cf99e9d02
Denize <DenizeScheffold@gmail.com>
2022-02-02 14:50:55 +0100

    change teacher id mock data

--- demo/src/main/java/com/example/demo/register/course/CourseAndStudentConfig.java
+++ demo/src/main/java/com/example/demo/register/course/CourseAndStudentConfig.java
@@ -24,8 +24,8 @@ public class CourseAndStudentConfig {
             CoursAndStudent denise1 = new CoursAndStudent(2,3);
             CoursAndStudent denise2 = new CoursAndStudent(3,3);
 
-            CoursAndStudent mikael  = new CoursAndStudent(1,3L);
-            CoursAndStudent stefan = new CoursAndStudent(2,3L);
+            CoursAndStudent mikael  = new CoursAndStudent(1,1L);
+            CoursAndStudent stefan = new CoursAndStudent(2,2L);
             CoursAndStudent kisslo = new CoursAndStudent(3,3L);
             repository.saveAll(List.of(jonte,jonte1,jonte2,denize,denize1,denize2,denise,denise1,denise2, mikael, kisslo,stefan));
 
